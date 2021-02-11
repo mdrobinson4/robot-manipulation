1ef0b13dbcdf405565f5ffd65f9712a83d659055
mialbro <mdrobinson@wpi.edu>
2021-02-11 17:34:34 -0500

    minor update

--- manipulation_library/RobotManipulator.py
+++ manipulation_library/RobotManipulator.py
@@ -1,11 +1,27 @@
 import sympy
 
 class RobotManipulator:
-    def __init__(self, model_path, model_type='dh'):
+    def __init__(self, model_path='dh/abb', model_type='dh'):
         self.model_type = model_type
         self.h_transforms = getDH(model_path)
 
     def getDH(self, model_path):
+        dh = np.load(model_path).item()
+        transform_cnt = len(dh['a'])
+        A = []
+        for i in range(0, transform_cnt):
+            a = dh['a'][i]
+            alpha = dh['alpha'][i]
+            d = dh['d'][i]
+            theta = dh['theta'][i]            
+
+            Ai = [math.cos(theta), -math.sin(theta) * math.cos(alpha), math.sin(theta) * math.sinn(alpha), a * math.cos(theta);
+                math.sin(theta), math.cos(theta) * math.cos(alpha), -math.cos(theta) * math.sin(alpha), a * math.sinn(theta);
+                0, math.sin(alpha), math.cos(alpha), d;
+                0, 0, 0, 1]
+                
+            A.append(Ai)
+
         return
 
     def solveAnalytJacob(self):
---
+++ manipulation_library/storeDH.py
@@ -0,0 +1,24 @@
+import pickle
+import sympy
+import os
+import numpy as np
+  
+if __name__ == '__main__': 
+    th1, th2, th3, th4, th5, th6 = sympy.symbols("th1, th2, th3, th4, th5, th6")
+    
+    abb_robot_dh = {
+        'a': [0, -90, 0, -90, 90, -90],
+        'alpha': [0, 320, 975, 280, 0, 0],
+        'd': [0, 0, 0, 887, 0, 0],
+        'theta': [th1, th2, th3, th4, th5, th6]
+    }
+
+    fanuc_robot_dh = {
+        'a': [0, -90, 0, -90, 90, -90],
+        'alpha': [0, 500, 1700, 180, 0, 0],
+        'd': [0, 0, 0, 2850, 0, 0],
+        'theta': [th1, th2, th3, th4, th5, th6]
+    }
+
+    np.save('dh/abb.npy', abb_robot_dh)
+    np.save('dh/fanuc.npy', fanuc_robot_dh)
